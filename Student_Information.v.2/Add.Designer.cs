a927b89755735274efb1bd227a55e9e3929c50f9
Bluhzzzz <rogermooresangol@gmail.com>
2021-04-29 13:27:55 +0700

    add table in add

--- Student_Information.v.2/Add.Designer.cs
+++ Student_Information.v.2/Add.Designer.cs
@@ -37,7 +37,7 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox6 = new System.Windows.Forms.ComboBox();
             this.comboBox7 = new System.Windows.Forms.ComboBox();
@@ -113,6 +113,7 @@
             this.pnlAdd_Subject.Name = "pnlAdd_Subject";
             this.pnlAdd_Subject.Size = new System.Drawing.Size(946, 781);
             this.pnlAdd_Subject.TabIndex = 1;
+            this.pnlAdd_Subject.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlAdd_Subject_Paint);
             // 
             // button7
             // 
@@ -146,7 +147,7 @@
             this.pnlAddStudent.Controls.Add(this.textBox2);
             this.pnlAddStudent.Controls.Add(this.button1);
             this.pnlAddStudent.Controls.Add(this.label4);
-            this.pnlAddStudent.Controls.Add(this.button2);
+            this.pnlAddStudent.Controls.Add(this.btnNext);
             this.pnlAddStudent.Controls.Add(this.comboBox1);
             this.pnlAddStudent.Controls.Add(this.comboBox6);
             this.pnlAddStudent.Controls.Add(this.comboBox7);
@@ -183,7 +184,7 @@
             this.pnlAddStudent.Controls.Add(this.textBox24);
             this.pnlAddStudent.Location = new System.Drawing.Point(1, 53);
             this.pnlAddStudent.Name = "pnlAddStudent";
-            this.pnlAddStudent.Size = new System.Drawing.Size(943, 754);
+            this.pnlAddStudent.Size = new System.Drawing.Size(943, 780);
             this.pnlAddStudent.TabIndex = 2;
             // 
             // label3
@@ -228,18 +229,19 @@
             this.label4.TabIndex = 119;
             this.label4.Text = "ADD STUDENTS";
             // 
-            // button2
+            // btnNext
             // 
-            this.button2.BackColor = System.Drawing.Color.MediumTurquoise;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(764, 717);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(127, 36);
-            this.button2.TabIndex = 101;
-            this.button2.Text = "Add";
-            this.button2.UseVisualStyleBackColor = false;
+            this.btnNext.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btnNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.ForeColor = System.Drawing.Color.White;
+            this.btnNext.Location = new System.Drawing.Point(764, 717);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(127, 36);
+            this.btnNext.TabIndex = 101;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
             // 
             // comboBox1
             // 
@@ -682,8 +684,8 @@
             this.BackColor = System.Drawing.Color.GhostWhite;
             this.ClientSize = new System.Drawing.Size(941, 833);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.pnlAdd_Subject);
             this.Controls.Add(this.pnlAddStudent);
+            this.Controls.Add(this.pnlAdd_Subject);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Add";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -711,7 +713,7 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label4;
-        public System.Windows.Forms.Button button2;
+        public System.Windows.Forms.Button btnNext;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ComboBox comboBox6;
         private System.Windows.Forms.ComboBox comboBox7;
--- Student_Information.v.2/Add.cs
+++ Student_Information.v.2/Add.cs
@@ -6,11 +6,14 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace Student_Information.v._2
 {
     public partial class Add : Form
     {
+        OleDbConnection con = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\Student_Info.accdb;Persist Security Info = True");
+      
         MainMenu main = new MainMenu();
         public Add(MainMenu f1)
         {
@@ -32,6 +35,26 @@ namespace Student_Information.v._2
 
         private void button5_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void pnlAdd_Subject_Paint(object sender, PaintEventArgs e)
+        {
+            OleDbDataAdapter adapt = new OleDbDataAdapter("Select * from [Subject]", con);
+            DataTable table = new DataTable();
+            adapt.Fill(table);
+            dgvSubject.DataSource = table;
+
+            dgvSubject.AllowUserToAddRows = false;
+            dgvSubject.EditMode = DataGridViewEditMode.EditProgrammatically;
+            dgvSubject.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            pnlAddStudent.Hide();
+            pnlAdd_Subject.Show();
 
         }
     }
